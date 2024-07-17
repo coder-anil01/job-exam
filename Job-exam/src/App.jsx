8713c13071cf19622b867adaacec763b13eba4b7
coder-anil01 <145540918+coder-anil01@users.noreply.github.com>
2024-07-17 12:29:50 +0530

    final

--- Job-exam/src/App.jsx
+++ Job-exam/src/App.jsx
@@ -13,14 +13,14 @@ function App() {
 
   return (
     <>
-      {/* {ExamStart ? 
+      {ExamStart ? 
       <form className="input-form">
         <input type="i'd Number" required placeholder="i'd Number" className='input-field'/>
         <input type="number" required placeholder="Enter Register Number" className='input-field'/>
         <button className='Submt-button' onClick={sartExam}>Submit</button>
       </form>
-      : <Exampaper/>} */}
-      <Exampaper/>
+      : <Exampaper/>}
+      {/* <Exampaper/> */}
     </>
   )
 }
